dcba83d07f70b06c16ba855970d564dc82e03c0d
TLajaydev1 <TLajayreddy@gmail.com>
2024-12-11 12:00:47 +0530

    controller

---
+++ security/src/main/java/security/controller/security.java
@@ -0,0 +1,17 @@
+package security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class security {
+
+	@GetMapping("/message")
+	public String Message() {
+		return "Come to Move on";
+	}
+	
+}
